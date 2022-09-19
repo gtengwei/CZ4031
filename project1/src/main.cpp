2b9b05bbc657e7f926118e049678fa8ae743310a
gtengwei <67195159+gtengwei@users.noreply.github.com>
2022-09-19 23:37:17 +0800

    first draft of database in memory

---
+++ project1/src/main.cpp
@@ -0,0 +1,80 @@
+#include "memory_pool.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+  int BLOCKSIZE = 0;
+  int choice = 0;
+  std :: cout <<"Choose your block size: "<<endl;
+
+  while (choice != 1 && choice != 2)
+  {
+    std :: cout <<"1. 200 BYTES"<<endl;
+    std :: cout <<"2. 500 BYTES"<<endl;
+    std :: cin >> choice;
+
+    if (choice == 1)
+    {
+      BLOCKSIZE = 200;
+    }
+    else if (choice == 2)
+    {
+      BLOCKSIZE = 500;
+    }
+    else
+    {
+      std :: cout <<"Invalid choice. Please try again."<<endl;
+      cin.clear();
+    }
+  }
+
+
+  MemoryPool memory_pool(500*(pow(10,6)), int(BLOCKSIZE)); // 500 MB
+
+  // Open test data
+  std::ifstream file("../data/test_data.tsv");
+
+  if (file.is_open())
+  {
+    std::string line;
+    int recordNum = 1;
+    while (std::getline(file, line))
+    {
+      Record rec;
+      stringstream linestream(line);
+      string data;
+
+      strcpy(rec.tconst, line.substr(0, line.find("\t")).c_str());
+
+      std::getline(linestream, data, '\t');
+      linestream >> rec.averageRating >> rec.numVotes;
+
+      std::tuple<void *, std::size_t> record = memory_pool.allocate(sizeof(rec));
+
+      memcpy(std::get<0>(record) + std::get<1>(record), &rec, sizeof(rec));
+
+      std :: cout << "Reading line " << recordNum << " of movie data" << '\n';
+      std :: cout << "Size of a record: " << sizeof(rec) << " bytes" << '\n';
+      std :: cout << "Inserted record " << recordNum << " at address: " << std::get<0>(record) + std::get<1>(record) << '\n';
+
+      Record check = *(Record *)(std::get<0>(record) + std::get<1>(record));
+      std :: cout << check.tconst << '\n';
+
+      recordNum += 1;
+    }
+    file.close();
+  };
+
+  std :: cout << "Current blocks used: " << memory_pool.getAllocated() << " blocks" << '\n';
+  std :: cout << "Actual size used: " << memory_pool.getActualSizeUsed() << " bytes" << '\n';
+  std :: cout << "Total size occupied: " << memory_pool.getSizeUsed() << " bytes" << '\n';
+  return 0;
+}
---
+++ project1/src/memory_pool.h
@@ -0,0 +1,91 @@
+#ifndef MEMORY_POOL_H
+#define MEMORY_POOL_H
+
+#include <vector>
+#include <unordered_map>
+#include <tuple>
+
+// Defines a single movie record (read from data file).
+struct Record
+{
+  float averageRating; // Average rating of this movie.
+  int numVotes;        // Number of votes of this movie.
+  char tconst[10];     // ID of the movie.
+};
+
+class MemoryPool
+{
+public:
+  // =============== Methods ================ //
+
+  // Creates a new memory pool with the following parameters:
+  // maxPoolSize: Maximum size of the memory pool.
+  // blockSize: The fixed size of each block in the pool.
+  MemoryPool(std::size_t maxPoolSize, std::size_t blockSize);
+
+  // Allocate a new block from the memory pool. Returns false if error.
+  bool allocateBlock();
+
+  // Allocates a new chunk to the memory pool.
+  // Creates a new block if chunk is unable to fit in current free block.
+  // Returns a tuple with blockID and the record's offset within the block.
+  std::tuple<void *, std::size_t> allocate(std::size_t sizeRequired);
+
+  // Deallocates an existing record and block if block becomes empty. Returns false if error.
+  bool deallocate(void *blockAddress, std::size_t offset, std::size_t sizeToDelete);
+
+  // Returns the maximum size of this memory pool.
+  std::size_t getMaxPoolSize() const
+  {
+    return maxPoolSize;
+  }
+
+  // Returns the size of a block in memory pool.
+  std::size_t getBlockSize() const
+  {
+    return blockSize;
+  };
+
+  // Returns the size used in the current block.
+  std::size_t getBlockSizeUsed() const
+  {
+    return blockSizeUsed;
+  };
+
+  // Returns current size used in memory pool (total blocks size).
+  std::size_t getSizeUsed() const
+  {
+    return sizeUsed;
+  }
+
+  // Returns actual size of all records stored in memory pool.
+  std::size_t getActualSizeUsed() const
+  {
+    return actualSizeUsed;
+  }
+
+  // Returns number of currently allocated blocks.
+  int getAllocated() const
+  {
+    return allocated;
+  };
+
+  // Destructor
+  ~MemoryPool();
+
+private:
+  // =============== Data ================ //
+
+  std::size_t maxPoolSize;    // Maximum size allowed for pool.
+  std::size_t blockSize;      // Size of each block in pool in bytes.
+  std::size_t sizeUsed;       // Current size used up for storage (total block size).
+  std::size_t actualSizeUsed; // Actual size used based on records stored in storage.
+  std::size_t blockSizeUsed;  // Size used up within the curent block we are pointing to.
+
+  int allocated; // Number of currently allocated blocks.
+
+  void *pool;  // Memory pool reference.
+  void *block; // Current block pointer we are inserting to.
+};
+
+#endif
