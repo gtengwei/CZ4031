1f5d49239e79ff421443de25dd24b348eb34ace9
PhantomIntergration <mgh1208.29@gmail.com>
2022-09-28 20:05:21 +0800

    Create

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -11,70 +11,69 @@ using namespace std;
 
 int main()
 {
-  int BLOCKSIZE = 0;
-  int choice = 0;
-  std :: cout <<"Choose your block size: "<<endl;
+	int BLOCKSIZE = 0;
+	int choice = 0;
+	std ::cout << "Choose your block size: " << endl;
 
-  while (choice != 1 && choice != 2)
-  {
-    std :: cout <<"1. 200 BYTES"<<endl;
-    std :: cout <<"2. 500 BYTES"<<endl;
-    std :: cin >> choice;
+	while (choice != 1 && choice != 2)
+	{
+		std ::cout << "1. 200 BYTES" << endl;
+		std ::cout << "2. 500 BYTES" << endl;
+		std ::cin >> choice;
 
-    if (choice == 1)
-    {
-      BLOCKSIZE = 200;
-    }
-    else if (choice == 2)
-    {
-      BLOCKSIZE = 500;
-    }
-    else
-    {
-      std :: cout <<"Invalid choice. Please try again."<<endl;
-      cin.clear();
-    }
-  }
+		if (choice == 1)
+		{
+			BLOCKSIZE = 200;
+		}
+		else if (choice == 2)
+		{
+			BLOCKSIZE = 500;
+		}
+		else
+		{
+			std ::cout << "Invalid choice. Please try again." << endl;
+			cin.clear();
+		}
+	}
 
+	MemoryPool memory_pool(500 * (pow(10, 6)), int(BLOCKSIZE)); // 500 MB
 
-  MemoryPool memory_pool(500*(pow(10,6)), int(BLOCKSIZE)); // 500 MB
+	// Open test data
+	std::ifstream file("../data/test_data.tsv");
 
-  // Open test data
-  std::ifstream file("../data/test_data.tsv");
+	if (file.is_open())
+	{
+		std::string line;
+		int recordNum = 1;
+		while (std::getline(file, line))
+		{
+			Record rec;
+			stringstream linestream(line);
+			string data;
 
-  if (file.is_open())
-  {
-    std::string line;
-    int recordNum = 1;
-    while (std::getline(file, line))
-    {
-      Record rec;
-      stringstream linestream(line);
-      string data;
+			strcpy(rec.tconst, line.substr(0, line.find("\t")).c_str());
 
-      strcpy(rec.tconst, line.substr(0, line.find("\t")).c_str());
+			std::getline(linestream, data, '\t');
+			linestream >> rec.averageRating >> rec.numVotes;
 
-      std::getline(linestream, data, '\t');
-      linestream >> rec.averageRating >> rec.numVotes;
+			std::tuple<void *, std::size_t> record = memory_pool.allocate(sizeof(rec));
 
-      std::tuple<void *, std::size_t> record = memory_pool.allocate(sizeof(rec));
+			memcpy(std::get<0>(record) + std::get<1>(record), &rec, sizeof(rec));
 
-      memcpy(std::get<0>(record) + std::get<1>(record), &rec, sizeof(rec));
+			std ::cout << "Reading line " << recordNum << " of movie data" << '\n';
+			std ::cout << "Size of a record: " << sizeof(rec) << " bytes" << '\n';
+			std ::cout << "Inserted record " << recordNum << " at address: " << std::get<0>(record) + std::get<1>(record) << '\n';
 
-      std :: cout << "Reading line " << recordNum << " of movie data" << '\n';
-      std :: cout << "Size of a record: " << sizeof(rec) << " bytes" << '\n';
-      std :: cout << "Inserted record " << recordNum << " at address: " << std::get<0>(record) + std::get<1>(record) << '\n';
+			Record check = *(Record *)(std::get<0>(record) + std::get<1>(record));
+			std ::cout << check.tconst << '\n';
 
-      Record check = *(Record *)(std::get<0>(record) + std::get<1>(record));
-      std :: cout << check.tconst << '\n';
+			recordNum += 1;
+		}
+		file.close();
+	};
 
-      recordNum += 1;
-    }
-    file.close();
-  };
-
-  std :: cout << "Current blocks used: " << memory_pool.getAllocated() << " blocks" << '\n';
-  std :: cout << "Actual size used: " << memory_pool.getActualSizeUsed() << " bytes" << '\n';
-  std :: cout << "Total size occupied: " << memory_pool.getSizeUsed() << " bytes" << '\n';
-  return 0;
+	std ::cout << "Current blocks used: " << memory_pool.getAllocated() << " blocks" << '\n';
+	std ::cout << "Actual size used: " << memory_pool.getActualSizeUsed() << " bytes" << '\n';
+	std ::cout << "Total size occupied: " << memory_pool.getSizeUsed() << " bytes" << '\n';
+	return 0;
 }
--- project1/src/memory_pool.cpp
+++ project1/src/memory_pool.cpp
@@ -12,103 +12,103 @@
 
 MemoryPool::MemoryPool(std::size_t maxPoolSize, std::size_t blockSize)
 {
-  this->maxPoolSize = maxPoolSize;
-  this->blockSize = blockSize;
-  this->sizeUsed = 0;
-  this->actualSizeUsed = 0;
-  this->allocated = 0;
+	this->maxPoolSize = maxPoolSize;
+	this->blockSize = blockSize;
+	this->sizeUsed = 0;
+	this->actualSizeUsed = 0;
+	this->allocated = 0;
 
-  // Create pool of blocks.
-  this->pool = operator new(maxPoolSize);
-  std::memset(pool, '\0', maxPoolSize); // Initialize pool all to null.
-  this->block = nullptr;
-  this->blockSizeUsed = 0;
+	// Create pool of blocks.
+	this->pool = operator new(maxPoolSize);
+	std::memset(pool, '\0', maxPoolSize); // Initialize pool all to null.
+	this->block = nullptr;
+	this->blockSizeUsed = 0;
 }
 
 // Methods
 
 bool MemoryPool::allocateBlock()
 {
-  // Only allocate a new block if we don't exceed maxPoolSize.
-  if (sizeUsed + blockSize <= maxPoolSize)
-  {
-    // Update variables
-    sizeUsed += blockSize;
-    block = pool + allocated * blockSize; // Set current block pointer to new block.
-    blockSizeUsed = 0;                    // Reset offset to 0.
+	// Only allocate a new block if we don't exceed maxPoolSize.
+	if (sizeUsed + blockSize <= maxPoolSize)
+	{
+		// Update variables
+		sizeUsed += blockSize;
+		block = pool + allocated * blockSize; // Set current block pointer to new block.
+		blockSizeUsed = 0;					  // Reset offset to 0.
 
-    allocated += 1;
+		allocated += 1;
 
-    std::cout << "------- New block allocated! -------" << '\n';
+		std::cout << "------- New block allocated! -------" << '\n';
 
-    return true;
-  }
-  else
-  {
-    std::cout << "Error: No memory left to allocate new block (" << sizeUsed << "/" << maxPoolSize << " used)." << '\n';
-    return false;
-  }
+		return true;
+	}
+	else
+	{
+		std::cout << "Error: No memory left to allocate new block (" << sizeUsed << "/" << maxPoolSize << " used)." << '\n';
+		return false;
+	}
 }
 
 std::tuple<void *, std::size_t> MemoryPool::allocate(std::size_t sizeRequired)
 {
-  // If record size exceeds block size, throw an error.
-  if (sizeRequired > blockSize)
-  {
-    std::cout << "Error: Size required larger than block size (" << sizeRequired << " vs " << blockSize << ")! Increase block size to store data." << '\n';
-    throw std::invalid_argument("Requested size too large!");
-  }
+	// If record size exceeds block size, throw an error.
+	if (sizeRequired > blockSize)
+	{
+		std::cout << "Error: Size required larger than block size (" << sizeRequired << " vs " << blockSize << ")! Increase block size to store data." << '\n';
+		throw std::invalid_argument("Requested size too large!");
+	}
 
-  // If no current block, or record can't fit into current block, make a new block.
-  if (allocated == 0 || blockSizeUsed + sizeRequired > blockSize)
-  {
-    bool isSuccessful = allocateBlock();
-    if (!isSuccessful)
-    {
-      throw std::logic_error("Failed to allocate new block!");
-    }
-  }
+	// If no current block, or record can't fit into current block, make a new block.
+	if (allocated == 0 || blockSizeUsed + sizeRequired > blockSize)
+	{
+		bool isSuccessful = allocateBlock();
+		if (!isSuccessful)
+		{
+			throw std::logic_error("Failed to allocate new block!");
+		}
+	}
 
-  // Update variables
-  std::size_t offset = blockSizeUsed;
+	// Update variables
+	std::size_t offset = blockSizeUsed;
 
-  blockSizeUsed += sizeRequired;
-  actualSizeUsed += sizeRequired;
+	blockSizeUsed += sizeRequired;
+	actualSizeUsed += sizeRequired;
 
-  // Return the new memory space to put in the record.
-  std::tuple<void *, std::size_t> recordAddress(block, offset);
+	// Return the new memory space to put in the record.
+	std::tuple<void *, std::size_t> recordAddress(block, offset);
 
-  return recordAddress;
+	return recordAddress;
 }
 
 bool MemoryPool::deallocate(void *blockAddress, std::size_t offset, std::size_t sizeToDelete)
 {
-  try
-  {
-    // Remove record from block.
-    void *addressToDelete = blockAddress + offset;
-    std::memset(addressToDelete, '\0', sizeToDelete);
+	try
+	{
+		// Remove record from block.
+		void *addressToDelete = blockAddress + offset;
+		std::memset(addressToDelete, '\0', sizeToDelete);
 
-    // Update actual size used.
-    actualSizeUsed -= sizeToDelete;
+		// Update actual size used.
+		actualSizeUsed -= sizeToDelete;
 
-    // If block is empty, just remove the size of the block (but don't deallocate block!).
-    unsigned char testblock[sizeToDelete];
-    memset(testblock, '\0', sizeToDelete);
+		// If block is empty, just remove the size of the block (but don't deallocate block!).
+		unsigned char testblock[sizeToDelete];
+		memset(testblock, '\0', sizeToDelete);
 
-    // Block is empty, remove size of block.
-    if (memcmp(testblock, blockAddress, sizeToDelete) == 0)
-    {
-      sizeUsed -= blockSize;
-    }
+		// Block is empty, remove size of block.
+		if (memcmp(testblock, blockAddress, sizeToDelete) == 0)
+		{
+			sizeUsed -= blockSize;
+		}
 
-    return true;
-  }
-  catch (...)
-  {
-    std::cout << "Error: Could not remove record/block at given address (" << blockAddress << ") and offset (" << offset << ")." << '\n';
-    return false;
-  };
+		return true;
+	}
+	catch (...)
+	{
+		std::cout << "Error: Could not remove record/block at given address (" << blockAddress << ") and offset (" << offset << ")." << '\n';
+		return false;
+	};
 }
 
 MemoryPool::~MemoryPool(){};
