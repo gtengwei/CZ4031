d0457c43f8b0e390d4d51382dc0118b74c292ee8
gtengwei <gohtengwei1999@hotmail.com>
2022-11-02 18:32:37 +0800

    refactorise database connection so that we only have to connect once

--- preprocessing.py
+++ preprocessing.py
@@ -553,3 +553,34 @@ def get_diff(json_obj_A, json_obj_B):
             diff_str = diff_str + "-"*200 + "\n"
     return diff_str
 
+class Database:
+    def __init__(self, host, database, user, password):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.connection = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
+        self.cur = self.connection.cursor()
+    
+
+    def get_query_result(self, query):
+        print("Query:", query)
+        
+            # host="localhost",
+            # database="TCP-H",
+            # user="postgres",
+            # password="1234")
+
+        #cur = conn.cursor()
+
+        self.cur.execute(query)
+        rows = self.cur.fetchall()
+
+        return rows[0][0][0]
+
+    def execute_query(self, query):
+        print("Query:", query)
+
+        self.cur.execute(query)
+        self.connection.commit()
+        print("Query executed successfully")
